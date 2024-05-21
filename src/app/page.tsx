88553f10cc3403daca901d76d34b7876291e3b21
papeixoto <pazevedopeixoto@gmail.com>
2024-05-21 15:38:55 +0100

    database setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { db } from "~/server/db";
+
 const mockUrls = [
   "https://utfs.io/f/5170dc2e-aaa4-4435-81c3-c0069ab5edbe-1zbfv.png",
   "https://utfs.io/f/abc34344-ee5f-4229-abfa-4ed7ffb62241-4yuf94.15.50-transformed.png",
@@ -11,12 +13,20 @@ const mockImages = mockUrls.map((url, index) => ({
   url,
 }));
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+  console.log(posts);
+
   return (
     <main>
       <div className="flex flex-wrap gap-4">
-        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
-          <div key={image.id} className="w-48">
+        {posts.map((post) => (
+          <div key={post.id} className="w-48">
+            {post.name}
+          </div>
+        ))}
+        {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
+          <div key={`${image.id}-${index}`} className="w-48">
             <img src={image.url} alt="gallery" />
           </div>
         ))}
