a9f670775ddca32b4c862c00d373548c279982e4
papeixoto <pazevedopeixoto@gmail.com>
2024-05-21 15:59:43 +0100

    reading images from the db to the UI

--- README.md
+++ README.md
@@ -5,8 +5,8 @@
 - [x] Make it deploy
 - [x] Scaffold basic UI with mock data
 - [x] Tidy up build process (turbo when running dev + don't check ts + eslint on next build)
-- [ ] Actually set up a database (supabase)
-- [ ] Attach database to UI
+- [x] Actually set up a database (supabase / postgres next)
+- [x] Attach database to UI
 - [ ] Add authentication (clerk)
 - [ ] Add image upload
 - [ ] Error management (sentry)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,34 +4,19 @@ import { db } from "~/server/db";
 // this makes it so whe there's a change to the db it knows to rebuild the page
 export const dynamic = "force-dynamic";
 
-const mockUrls = [
-  "https://utfs.io/f/5170dc2e-aaa4-4435-81c3-c0069ab5edbe-1zbfv.png",
-  "https://utfs.io/f/abc34344-ee5f-4229-abfa-4ed7ffb62241-4yuf94.15.50-transformed.png",
-  "https://utfs.io/f/4c19f4e2-3b5a-46d1-b9ae-143d75e942e6-4yuf94.16.37-transformed.png",
-  "https://utfs.io/f/415b3a93-45ad-4ffa-bee4-1b5518ca510a-4yuf94.16.58-transformed.png",
-  "https://utfs.io/f/2c484b7d-7398-4493-bc0a-fae55d946d2d-4yuf94.16.14-transformed.png",
-];
-
-const mockImages = mockUrls.map((url, index) => ({
-  id: index,
-  url,
-}));
-
 export default async function HomePage() {
-  const posts = await db.query.posts.findMany();
-  console.log(posts);
+  const images = await db.query.images.findMany({
+    orderBy: (model, { desc }) => desc(model.id),
+  });
+  console.log(images);
 
   return (
     <main>
       <div className="flex flex-wrap gap-4">
-        {posts.map((post) => (
-          <div key={post.id} className="w-48">
-            {post.name}
-          </div>
-        ))}
-        {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
-          <div key={`${image.id}-${index}`} className="w-48">
+        {[...images, ...images, ...images].map((image, index) => (
+          <div key={`${image.id}-${index}`} className="flex w-48 flex-col">
             <img src={image.url} alt="gallery" />
+            <div>{image.name}</div>
           </div>
         ))}
       </div>
