f43d374f76905963f3293a253a11fe3ac817517c
papeixoto <pazevedopeixoto@gmail.com>
2024-05-22 16:20:34 +0100

    upload thing with multiple files + ssr

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -24,6 +24,15 @@ export const images = createTable(
     id: serial("id").primaryKey(),
     name: varchar("name", { length: 256 }).notNull(),
     url: varchar("url", { length: 1024 }).notNull(),
+
+    // this was added after the table creation
+    // the right way to migrate it:
+    // 1. make this field nullable
+    // 2. push the changes to the db
+    // 3. backfill all the data
+    // 4. make the field not nullable
+    userId: varchar("userId", { length: 256 }).notNull(),
+
     createdAt: timestamp("created_at", { withTimezone: true })
       .default(sql`CURRENT_TIMESTAMP`)
       .notNull(),
