528c0b7fb8d5ca4300fe108c4559b59e1905ec33
papeixoto <pazevedopeixoto@gmail.com>
2024-05-22 16:53:45 +0100

    break queries into separate file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,12 @@
 import { SignedIn, SignedOut } from "@clerk/nextjs";
-import { db } from "~/server/db";
+import { getMyImages } from "~/server/queries";
 
 // by default next will build the page just once and then serve it to the user
 // this makes it so whe there's a change to the db it knows to rebuild the page
 export const dynamic = "force-dynamic";
 
 async function Images() {
-  const images = await db.query.images.findMany({
-    orderBy: (model, { desc }) => desc(model.id),
-  });
+  const images = await getMyImages();
 
   return (
     <div className="flex flex-wrap gap-4">
