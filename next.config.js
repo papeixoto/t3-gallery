ef2020586ffd560d14a3ad1060ccd61e4269fa16
papeixoto <pazevedopeixoto@gmail.com>
2024-05-23 16:51:49 +0100

    posthog event capture

--- next.config.js
+++ next.config.js
@@ -19,11 +19,11 @@ const coreConfig = {
     return [
       {
         source: "/ingest/static/:path*",
-        destination: "https://us-assets.i.posthog.com/static/:path*",
+        destination: "https://eu-assets.i.posthog.com/static/:path*",
       },
       {
         source: "/ingest/:path*",
-        destination: "https://us.i.posthog.com/:path*",
+        destination: "https://eu.i.posthog.com/:path*",
       },
     ];
   },
