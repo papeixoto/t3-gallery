8be23b6df9c17d9c870b49834a9cd28573b2adbc
papeixoto <pazevedopeixoto@gmail.com>
2024-05-23 17:10:37 +0100

    event when deleting image

--- src/server/queries.ts
+++ src/server/queries.ts
@@ -6,6 +6,7 @@ import { images } from "./db/schema";
 import { and, eq } from "drizzle-orm";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import analyticsServerClient from "./analytics";
 
 export async function getMyImages() {
   const user = auth();
@@ -44,6 +45,14 @@ export async function deleteImage(id: number) {
   await db
     .delete(images)
     .where(and(eq(images.id, id), eq(images.userId, user.userId)));
+
+  analyticsServerClient.capture({
+    distinctId: user.userId,
+    event: "delete image",
+    properties: {
+      imageId: id,
+    },
+  });
 
   redirect("/");
 }
